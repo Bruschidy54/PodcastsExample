bfd762cd41976935cbcf2d809634f5dac881ff67
Dylan Bruschi <dylanbruschi54@gmail.com>
2018-08-02 21:00:56 -0400

    Create SearchPodcastController with TableView and SearchBar
    [Ticket: X]

---
+++ PodcastsExample/Controllers/PodcastSearchController.swift
@@ -0,0 +1,9 @@
+//
+//  PodcastSearchController.swift
+//  PodcastsExample
+//
+//  Created by Dylan Bruschi on 8/2/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
