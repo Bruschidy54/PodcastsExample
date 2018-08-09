a46baae6e27043285ad4d47c2466e5175cb0e909
Dylan Bruschi <dylanbruschi54@gmail.com>
2018-08-08 20:55:43 -0400

    Use FeedKit to parse XML file of Podcast episode RSS feed
    [Ticket: X]

---
+++ PodcastsExample/Controllers/EpisodesController.swift
@@ -0,0 +1,9 @@
+//
+//  EpisodesController.swift
+//  PodcastsExample
+//
+//  Created by Dylan Bruschi on 8/8/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
---
+++ PodcastsExample/Models/Episode.swift
@@ -0,0 +1,9 @@
+//
+//  Episode.swift
+//  PodcastsExample
+//
+//  Created by Dylan Bruschi on 8/8/18.
+//  Copyright © 2018 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
